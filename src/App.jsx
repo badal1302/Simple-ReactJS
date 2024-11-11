5aaf666fedac75fb1de05d2653ee466964f885e2
badal1302 <badalpatel539@gmail.com>
2024-11-10 18:47:18 -0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import FunctionalComp from './FunctionalCompunctionalcomp'
+import FunctionalComp from './FunctionalComp'
 import ClassComp from './ClassComplasscomp'
 import LifecycleExample from './LifecycleExample'
 
